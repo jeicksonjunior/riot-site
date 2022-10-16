10c989d6713a21401f5058ad923135a654482504
jeicksonjunior <jeickson_lima@hotmail.com>
2022-10-16 19:50:25 -0300

    16/10/22-5

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,1 +1,29 @@
-$('.carrosel').slick();
+
+$('.gallery').slick({
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 3
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+        }
+    ]
+});
